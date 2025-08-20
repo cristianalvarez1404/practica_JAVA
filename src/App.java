69b9c925013cd10d69c8b7fe0855beb6905b3942
cristian <cristianalvarez1496@gmail.com>
2025-08-20 14:26:56 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //comentarios
+        /* 
+         * Comentario 1
+         * Comentario 2
+        */
+    }
+}
